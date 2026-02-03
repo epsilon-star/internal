32306fb2efd1828cbf33b758b220abf278526f97
epsilon-star <mobinbaratian18@gmail.com>
2026-02-03 12:46:19 +0330

    fixing ip addr 2

--- internal/settings.py
+++ internal/settings.py
@@ -26,7 +26,7 @@ SECRET_KEY = 'django-insecure-huiv(jcmsn*6qd3*63bka2ci%!=07r0d!uhwu$i)2%i0l3!ij5
 DEBUG = True
 
 ALLOWED_HOSTS = [
-    'panel.mobinbaratian.ir'
+    'panel.mobinbaratian.ir',
     # '192.168.1.102',
     '51.161.30.148',
     'localhost',
